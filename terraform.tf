7670a04c6d1f5705dd0d6d790aa3afd73831a657
mouhamadou.gueye <mouhamadou.gueye.auditeur@lecnam.net>
2023-10-14 14:10:07 +0200

    My second  commit

--- terraform.tf
+++ terraform.tf
@@ -1,18 +1,18 @@
 
 data "aws_ami" "my_ubuntu_ami" {
-    most_recent = true
-    owners = [ "self" ]
-    filter {
-      name = "name"
-      values = [ "my_ubuntu_ami" ]
-    }
-  
+  most_recent = true
+  owners      = ["self"]
+  filter {
+    name   = "name"
+    values = ["my_ubuntu_ami"]
+  }
+
 }
 
 # create ec2 instance from image build with packer
 resource "aws_instance" "ubuntu" {
-  ami = data.aws_ami.my_ubuntu_ami.id
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.my_ubuntu_ami.id
+  instance_type = "t2.large"
   tags = {
     "Name" = "my_ubuntu_ami"
   }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region where resources will be deployed"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "profile" {
   description = "AWS profile used"
-  type = string
-  default = "default"
+  type        = string
+  default     = "default"
 }
